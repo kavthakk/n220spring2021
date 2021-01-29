d8774f13a70a7b01c79b3ded781dd764d9c01b4b
kavthakk <77465190+kavthakk@users.noreply.github.com>
2021-01-28 21:24:55 -0600

    adding variables

--- pcomp/js/app.js
+++ pcomp/js/app.js
@@ -1,55 +1,59 @@
 function setup() {
     createCanvas(500,500);
 }
-
-
 function draw() {
-    background(11,28,71);
-
+    
+    let navy = color(11, 28, 71);
+    let xPos = 250;
+    let yPos = 250;
+    
+    background(navy);
+    
+    
     //light blue circle
     noStroke();
     fill(3, 252, 232);
-    circle (250, 250, 250);
+    circle (xPos, yPos, 250);
 
     //white circle with dark green stroke
     fill(255, 255, 255);
     strokeWeight(20);
     stroke(11, 71, 11)
-    circle (250, 250, 200);
+    circle (xPos, yPos, 200);
 
     //black rectangle in middle
     noStroke();
     fill(0,0,0);
-    rect(60,237.5,380,25);
+    rect(60,240,380,20);
 
     //pinkish line across
     strokeWeight(10);
     stroke(255, 150, 150);
-    line(0, 250, 500, 250);
+    line(0, yPos, 500, yPos);
 
     //tall K
     noStroke();
-    fill(11,28,71);
+    fill(navy);
     rect(115, 120, 20, 260, 10);
 
     //tall T
     noStroke();
-    fill(11,28,71);
-    rect(305, 125, 20, 250, 10);
+    fill(navy);
+    rect(305, 125, 20, xPos, 10);
 
     //top K
     strokeWeight(20);
-    stroke(11,28,71);
-    line(135, 250, 225, 130);
+    stroke(navy);
+    line(135, yPos, 225, 130);
 
     //bottom K
     strokeWeight(20);
-    stroke(11,28,71);
-    line(135, 250, 225, 370);
+    stroke(navy);
+    line(135, yPos, 225, 370);
 
     //top T
     strokeWeight(20);
-    stroke(11,28,71);
-    line(250, 130, 380, 130);
+    stroke(navy);
+    line(xPos, 130, 380, 130);
 
 }
