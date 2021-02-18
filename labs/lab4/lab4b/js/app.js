9c15790853bb8018188d9410eade0f346979a7fc
kavthakk <77465190+kavthakk@users.noreply.github.com>
2021-02-17 20:53:41 -0500

    fix naming

--- labs/lab4/lab4b/js/app.js
+++ labs/lab4/lab4b/js/app.js
@@ -1,16 +1,30 @@
-//Personal Composition
-let colorWheel = ["#0a003b", "#ffc2c2", "#047000", "#0a003b", "#ffc2c2", "#047000", "#0a003b", "#ffc2c2", "#047000", "#0a003b", "#ffc2c2", "#047000", "#0a003b", "#ffc2c2", "#047000", "#0a003b", "#ffc2c2", "#047000"];
-function setup() {
-  createCanvas(600, 600);
+var value = 10;
+let x = [];
+let y = [];
 
-  for (i = 0; i < colorWheel.length; i++) {
-    noStroke();
-    fill(colorWheel[i]);
-    rect(100 * i, 100 * i, 300, 300);
-    circle(50 * i, 50 * i, 50);
-    circle(50 * i, 100 * i, 25);
-    circle(100 * i, 50 * i, 25);
-    circle(50 * i, i + 10, 15);
-    circle(i + 10, 50 * i, 15);
-  }
+function setup() {
+    createCanvas(600,600);
+    noStroke()
+
+    for (var i=0; i < value; i++) {
+        x[i] = 0;
+        y[i] = 0;
+    }
+}
+
+function draw() {
+    background(77, 45, 11);
+
+    x.push(mouseX);
+    x.shift();
+
+    y.push(mouseY);
+    y.shift();
+
+    for (var i = 0; i < value; i++) {
+        fill(0, 194, 16);
+        circle(x[i], y[i], 20);
+        fill(0, 77, 10);
+        ellipse(x[i], y[i], 25, 40);
+    }
 }
