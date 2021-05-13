a6e76e2b3a5520b9e4f50c162ef27786cb60f0f2
kavthakk <77465190+kavthakk@users.noreply.github.com>
2021-05-13 12:05:30 -0500

    made the color mixer cooler

--- labs/lab10/lab10b/js/app.js
+++ labs/lab10/lab10b/js/app.js
@@ -18,10 +18,12 @@ var green10 = document.getElementById("green10");
 var blue1 = document.getElementById("blue1");
 var blue5 = document.getElementById("blue5");
 var blue10 = document.getElementById("blue10");
+var reset = document.getElementById("reset");
 
 let redVal = 0;
 let greenVal = 0;
 let blueVal = 0;
+let luminance = 0;
 
 red1.addEventListener("click", redChange);
 red5.addEventListener("click", redChange);
@@ -32,22 +34,47 @@ green10.addEventListener("click", greenChange);
 blue1.addEventListener("click", blueChange);
 blue5.addEventListener("click", blueChange);
 blue10.addEventListener("click", blueChange);
+reset.addEventListener("click", resetMixer);
 
 function redChange(event) {
   redAdd = Number(event.target.getAttribute("data-red"));
   redVal += redAdd;
   block.style.backgroundColor = "rgb(" + redVal + ","+ greenVal + ","+ blueVal + ")";
+  block.innerHTML = block.style.backgroundColor;
   colorCode.innerHTML = block.style.backgroundColor;
+  luminance = (redVal*0.2126) + (greenVal*0.7152) + (blueVal*0.0722);
+  //(0.2126*R + 0.7152*G + 0.0722*B)
+  if (luminance >= 200) {
+    block.style.color = "rgb(" + 0 + ","+ 0 + ","+ 0 + ")";
+    console.log("luminated!");
+  }
 }
 function greenChange(event) {
   greenAdd = Number(event.target.getAttribute("data-green"));
   greenVal += greenAdd;
   block.style.backgroundColor = "rgb(" + redVal + ","+ greenVal + ","+ blueVal + ")";
+  block.innerHTML = block.style.backgroundColor;
   colorCode.innerHTML = block.style.backgroundColor;
+  luminance = (redVal*0.2126) + (greenVal*0.7152) + (blueVal*0.0722);
+  //(0.2126*R + 0.7152*G + 0.0722*B)
+  if (luminance >= 200) {
+    block.style.color = "rgb(" + 0 + ","+ 0 + ","+ 0 + ")";
+    console.log("luminated!");
+  }
 }
 function blueChange(event) {
   blueAdd = Number(event.target.getAttribute("data-blue"));
   blueVal += blueAdd;
   block.style.backgroundColor = "rgb(" + redVal + ","+ greenVal + ","+ blueVal + ")";
+  block.innerHTML = block.style.backgroundColor;
   colorCode.innerHTML = block.style.backgroundColor;
+  luminance = (redVal*0.2126) + (greenVal*0.7152) + (blueVal*0.0722);
+  //(0.2126*R + 0.7152*G + 0.0722*B)
+  if (luminance >= 200) {
+    block.style.color = "rgb(" + 0 + ","+ 0 + ","+ 0 + ")";
+    console.log("luminated!");
+  }
+}
+function resetMixer() {
+  location.reload();
 }
